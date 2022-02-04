1f349b9a8803ca84f07930e9e602a4370d4be42f
lucasbat20 <lucas.batier@hotmail.fr>
2022-02-04 18:37:38 +0100

    Set production to teasing page

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
 
     const production = process.env.NODE_ENV === 'production';
 
-    if (false) {
+    if (production) {
         return (
             <ThemeProvider theme={theme}>
                 <CssBaseline />
