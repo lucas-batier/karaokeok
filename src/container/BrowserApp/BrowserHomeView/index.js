ca7ed8e851d0b4cb31ed9405313bba6dc8d548eb
lucasbat20 <lucas.batier@hotmail.fr>
2022-02-07 19:45:45 +0100

    Update Home message

--- src/container/BrowserApp/BrowserHomeView/index.js
+++ src/container/BrowserApp/BrowserHomeView/index.js
@@ -1,20 +1,29 @@
 import React from "react";
-import {Box, useTheme, Grid, Typography} from "@mui/material";
+import {Box, useTheme, Grid, Typography, ButtonBase} from "@mui/material";
 import BrowserApp from "../index";
 import {LibraryMusicRounded, AddReactionRounded, LoginRounded} from "@mui/icons-material";
 import ColumnButtonCard from "../../../components/ColumnButtonCard";
+import {useLinkStyles} from "../../../styles/link";
 
 
 function BrowserHomeView() {
     const theme = useTheme();
+    const linkClasses = useLinkStyles();
 
     return (
         <BrowserApp title={"KaraokeOK"}>
             <Box display={"flex"} justifyContent={"center"} m={3}>
                 <Grid container mr={6} columnSpacing={6} justifyContent={"space-between"} alignItems={"center"}>
                     <Grid item xs={12}>
-                        <Typography variant={"h4"}>
-                            Prêt à vous casser la voix ?!
+                        <Typography variant={"h6"} paragraph>
+                            T’en as marre de passer plus de temps à choisir le prochain karaoke plutôt qu’à le chanter ?
+                        </Typography>
+                        <Typography variant={"h6"} paragraph>
+                            Essaye <ButtonBase component={"a"} href={"/"} className={linkClasses.primary}>KaraokeOK</ButtonBase>
+                            , première application communautaires avec des versions Karaoke vérifiés !
+                        </Typography>
+                        <Typography variant={"h6"} paragraph>
+                            Prêt à te casser la voix ?
                         </Typography>
                     </Grid>
                     <Grid item>
